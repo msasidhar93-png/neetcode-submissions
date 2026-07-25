9804f624418473413bc918e7ed52fb16a45b122f
msasidhar93-png <msasidhar93@gmail.com>
2026-07-25 13:35:48 -0400

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i=0;
+        int j= s.length()-1;
+        boolean result = false;
+        if(s.length()<=1){
+            return true;
+        }
+        while(i<j && s.length()>1){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i)) == Character.toLowerCase(s.charAt(j))){
+                System.out.println("i"+ i + s.charAt(i)+ "j"+ j + s.charAt(j));
+                result = true;
+                i++;
+                j--;
+            }else{
+                System.out.println("i"+ i + s.charAt(i)+ "j"+ j + s.charAt(j));
+                return false;
+            }
+
+        }
+        return result;
+    }
+}
